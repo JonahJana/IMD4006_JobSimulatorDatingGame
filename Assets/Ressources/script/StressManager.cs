9a852123d11d9a586075f6b04b4b4295c04a45a6
Jonah <jonahjanakovic@cmail.carleton.ca>
2019-12-04 10:54:18 -0500

    stress bar has been completed.

--- Assets/Ressources/script/StressManager.cs
+++ Assets/Ressources/script/StressManager.cs
@@ -9,6 +9,10 @@ public class StressManager : MonoBehaviour
     public GameObject stressBar;
     public float startingWidth;
 
+    public float currentSize;
+
+    public Text barWidth;
+
 
     //temporary
     public Button addStress;
@@ -21,22 +25,39 @@ public class StressManager : MonoBehaviour
 
         addStress.onClick.AddListener(addStrs);
         removeStress.onClick.AddListener(rStrs);
+
+        updateWidthTxt(startingWidth);
+
+        currentSize = 100;
+    }
+
+    void updateWidthTxt(float gibenWidth)
+    {
+        barWidth.text = "" + gibenWidth;
     }
 
     void decreaseStress(float percent)
     {
-        float widthDelta = -1 * ((startingWidth / 100) * percent);
+        currentSize = currentSize - percent;
+
+        float widthDelta = (startingWidth / 100) * currentSize;
 
         var stressBarTransform = stressBar.transform as RectTransform;
         stressBarTransform.sizeDelta = new Vector2(widthDelta, stressBarTransform.sizeDelta.y);
+
+        updateWidthTxt(widthDelta);
     }
 
     void increaseStress(float percent)
     {
-        float widthDelta = 1 * ((startingWidth / 100) * percent);
+        currentSize = currentSize + percent;
+
+        float widthDelta = (startingWidth / 100) * currentSize;
 
         var stressBarTransform = stressBar.transform as RectTransform;
         stressBarTransform.sizeDelta = new Vector2(widthDelta, stressBarTransform.sizeDelta.y);
+
+        updateWidthTxt(widthDelta);
     }
 
     void addStrs()
