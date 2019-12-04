f8ca5d77bfed92383a1fc7017d751126271bcbe4
Leostorm <etienne.doucet.leger@gmail.com>
2019-12-04 02:46:24 -0500

    finished adding cindy tree boy halfway

--- Assets/Ressources/script/DialogueBox.cs
+++ Assets/Ressources/script/DialogueBox.cs
@@ -35,6 +35,10 @@ public class DialogueBox : MonoBehaviour
 
     public GUIStyle customStyle;
 
+
+    int Cindypoint = 0;
+    int CindySeducepoint = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -54,7 +58,7 @@ public class DialogueBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     if(Sequence == NextSquence)
+        if (Sequence == NextSquence)
         {
             //Debug.Log("Run once");
             name_interviewer = parser.GetName(lineNum);
@@ -64,31 +68,31 @@ public class DialogueBox : MonoBehaviour
             interviewerName.text = name_interviewer;
             for (int i = 0; i < 4; i++)
             {
-                dialogue_btns = parser.GetContent(lineNum + 1 +i);
+                dialogue_btns = parser.GetContent(lineNum + 1 + i);
                 ButtonsChoice[i].GetComponentInChildren<Text>().text = dialogue_btns;
                 string choice = parser.GetChoice(lineNum + 1 + i);
-                ButtonsChoice[i].onClick.AddListener(delegate { test(choice);});
+                ButtonsChoice[i].onClick.AddListener(delegate { test(choice); });
             }
             Sequence = 0;
             NextSquence++;
         }
 
-     if(checkpls)
+        if (checkpls)
         {
             CheckOptions();
             checkpls = false;
         }
-        
+
 
 
     }
 
-     void OnGUI()
+    void OnGUI()
     {
         //dialogue = GUI.TextField(new Rect(100, 400, 600, 200), dialogue, customStyle);
     }
 
-     void test( string choice)
+    void test(string choice)
     {
         CurrentChoice = choice;
         checkpls = true;
@@ -102,13 +106,29 @@ public class DialogueBox : MonoBehaviour
         //Debug.Log("tester");
         switch (CurrentChoice)
         {
-            
+
             case "a02": lineNum = 5; Sequence = NextSquence; resetButon(); break;
             case "b01": lineNum = 10; Sequence = NextSquence; resetButon(); break;
-            case "b02": lineNum = 15; Sequence = NextSquence; resetButon(); break;
+            case "b02": addCindypoint(); lineNum = 15; Sequence = NextSquence; resetButon(); break;
             case "b03": lineNum = 20; Sequence = NextSquence; resetButon(); break;
-            case "b04": lineNum = 25; Sequence = NextSquence; resetButon(); break;
+            case "b04": addCindyFlirtpoint(); lineNum = 25; Sequence = NextSquence; resetButon(); break;
             case "c05": lineNum = 30; Sequence = NextSquence; resetButon(); break;
+            case "c01": addCindyFlirtpoint(); lineNum = 35; Sequence = NextSquence; resetButon(); break;
+            case "c02": lineNum = 40; Sequence = NextSquence; resetButon(); break;
+            case "c03": lineNum = 45; Sequence = NextSquence; resetButon(); break;
+            case "c04": addCindypoint(); lineNum = 50; Sequence = NextSquence; resetButon(); break;
+            case "d05": lineNum = 55; Sequence = NextSquence; resetButon(); break;
+            case "d01": addCindypoint(); lineNum = 60; Sequence = NextSquence; resetButon(); break;
+            case "d02": addCindyFlirtpoint(); lineNum = 65; Sequence = NextSquence; resetButon(); break;
+            case "d03": lineNum = 70; Sequence = NextSquence; resetButon(); break;
+            case "d04": lineNum = 75; Sequence = NextSquence; resetButon(); break;
+            case "e05": lineNum = 80; Sequence = NextSquence; resetButon(); break;
+            case "e01": lineNum = 85; Sequence = NextSquence; resetButon(); break;
+            case "e02": addCindypoint(); lineNum = 90; Sequence = NextSquence; resetButon(); break;
+            case "e03": addCindyFlirtpoint(); lineNum = 95; Sequence = NextSquence; resetButon(); break;
+            case "e04": lineNum = 100; Sequence = NextSquence; resetButon(); break;
+            case "f05": selectEndingCindy(); Sequence = NextSquence; resetButon(); break;
+            case "End05": lineNum = 120; Sequence = NextSquence; resetButon(); break;
 
         }
 
@@ -118,11 +138,37 @@ public class DialogueBox : MonoBehaviour
     {
         for (int i = 0; i < 4; i++)
         {
-           
+
             ButtonsChoice[i].onClick.RemoveAllListeners();
         }
     }
 
-   
+    void selectEndingCindy()
+    {
+        if (Cindypoint >= 3)
+        {
+            lineNum = 105;
+        }else if (CindySeducepoint >= 3)
+        {
+            lineNum = 115;
+        }
+        else
+        {
+            lineNum = 110;
+        }
+    }
+
+    void addCindypoint()
+    {
+        Cindypoint++;
+        Debug.Log(Cindypoint);
+    }
+
+    void addCindyFlirtpoint()
+    {
+        CindySeducepoint++;
+        Debug.Log(CindySeducepoint);
+    }
+
 
 }
