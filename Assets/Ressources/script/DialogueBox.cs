25571fbc2ecaa1133c94576d1568414d19ea72aa
Leostorm <etienne.doucet.leger@gmail.com>
2019-12-04 22:34:20 -0500

    added bg change tree boy is 3/4 in

--- Assets/Ressources/script/DialogueBox.cs
+++ Assets/Ressources/script/DialogueBox.cs
@@ -51,6 +51,13 @@ public class DialogueBox : MonoBehaviour
     int EastonpointSeduce = 0;
 
     bool firsttime = true;
+    bool akward = false;
+    bool loveMode = false;
+    //here we will handle all the transitionnal effects
+    public Image love;
+    public Image bgAkward;
+    public Image faceAkward;
+
 
     // Start is called before the first frame update
     void Start()
@@ -80,6 +87,7 @@ public class DialogueBox : MonoBehaviour
         }
         if (Sequence == NextSquence)
         {
+            
             //Debug.Log("Run once");
             name_interviewer = parser.GetName(lineNum);
             dialogue = parser.GetContent(lineNum);
@@ -131,30 +139,40 @@ public class DialogueBox : MonoBehaviour
             case "a02": lineNum = 5; Sequence = NextSquence; resetButon(); break;
             case "b01": lineNum = 10; Sequence = NextSquence; resetButon(); break;
             case "b02": addCindypoint(); lineNum = 15; Sequence = NextSquence; resetButon(); break;
-            case "b03": lineNum = 20; Sequence = NextSquence; resetButon(); break;
+            case "b03":  lineNum = 20; Sequence = NextSquence; resetButon(); badSitution(); break;
             case "b04": addCindyFlirtpoint(); lineNum = 25; Sequence = NextSquence; resetButon(); break;
-            case "c05": lineNum = 30; Sequence = NextSquence; resetButon(); break;
+            case "c05": lineNum = 30; Sequence = NextSquence; resetButon(); Resetbg(); break;
             case "c01": addCindyFlirtpoint(); lineNum = 35; Sequence = NextSquence; resetButon(); break;
-            case "c02": lineNum = 40; Sequence = NextSquence; resetButon(); break;
+            case "c02": lineNum = 40; Sequence = NextSquence; resetButon(); badSitution(); break;
             case "c03": lineNum = 45; Sequence = NextSquence; resetButon(); break;
             case "c04": addCindypoint(); lineNum = 50; Sequence = NextSquence; resetButon(); break;
-            case "d05": lineNum = 55; Sequence = NextSquence; resetButon(); break;
+            case "d05": lineNum = 55; Sequence = NextSquence; resetButon(); Resetbg(); break;
             case "d01": addCindypoint(); lineNum = 60; Sequence = NextSquence; resetButon(); break;
             case "d02": addCindyFlirtpoint(); lineNum = 65; Sequence = NextSquence; resetButon(); break;
             case "d03": lineNum = 70; Sequence = NextSquence; resetButon(); break;
-            case "d04": lineNum = 75; Sequence = NextSquence; resetButon(); break;
-            case "e05": lineNum = 80; Sequence = NextSquence; resetButon(); break;
+            case "d04": lineNum = 75; Sequence = NextSquence; resetButon(); badSitution(); break;
+            case "e05": lineNum = 80; Sequence = NextSquence; resetButon(); Resetbg(); break;
             case "e01": lineNum = 85; Sequence = NextSquence; resetButon(); break;
             case "e02": addCindypoint(); lineNum = 90; Sequence = NextSquence; resetButon(); break;
             case "e03": addCindyFlirtpoint(); lineNum = 95; Sequence = NextSquence; resetButon(); break;
-            case "e04": lineNum = 100; Sequence = NextSquence; resetButon(); break;
-            case "f05": selectEndingCindy(); Sequence = NextSquence; resetButon(); break;
-            case "End05": StartCoroutine(Fadeinout_bg()); lineNum = 120; Sequence = NextSquence; resetButon(); break;
-            case "g05":lineNum = 125; Sequence = NextSquence; resetButon(); break;
+            case "e04": lineNum = 100; Sequence = NextSquence; resetButon(); badSitution(); break;
+            case "f05": selectEndingCindy(); Sequence = NextSquence; resetButon(); Resetbg(); break;
+            case "End05": StartCoroutine(Fadeinout_bg()); lineNum = 120; Sequence = NextSquence; resetButon(); Resetbg(); break;
+            case "g05":lineNum = 125; Sequence = NextSquence; resetButon(); Resetbg(); break;
             case "g01": addEpoint(); lineNum = 130; Sequence = NextSquence; resetButon(); break;
             case "g02": lineNum = 135; Sequence = NextSquence; resetButon(); break;
-            case "g03": lineNum = 140; Sequence = NextSquence; resetButon(); break;
+            case "g03": lineNum = 140; Sequence = NextSquence; resetButon(); badSitution(); break;
             case "g04": addEFlirtpoint(); lineNum = 145; Sequence = NextSquence; resetButon(); break;
+            case "h05": lineNum = 150; Sequence = NextSquence; resetButon(); Resetbg(); break;
+            case "h01": lineNum = 155; Sequence = NextSquence; resetButon(); break;
+            case "h02": addEFlirtpoint(); lineNum = 160; Sequence = NextSquence; resetButon(); break;
+            case "h03":  addEpoint(); lineNum = 165; Sequence = NextSquence; resetButon(); break;
+            case "h04":  lineNum = 170; Sequence = NextSquence; resetButon(); badSitution(); break;
+            case "i05": lineNum = 175; Sequence = NextSquence; resetButon(); Resetbg(); break;
+            case "i01":addEpoint(); lineNum = 180; Sequence = NextSquence; resetButon(); break;
+            case "i02": addEFlirtpoint(); lineNum = 185; Sequence = NextSquence; resetButon(); break;
+            case "i03":  lineNum = 190; Sequence = NextSquence; resetButon(); break;
+            case "i04": lineNum = 195; Sequence = NextSquence; resetButon(); badSitution(); break;
 
         }
 
@@ -176,6 +194,7 @@ public class DialogueBox : MonoBehaviour
             lineNum = 105;
         }else if (CindySeducepoint >= 3)
         {
+            StartCoroutine(LoveIntheAir());
             lineNum = 115;
         }
         else
@@ -209,6 +228,7 @@ public class DialogueBox : MonoBehaviour
 
     void addCindyFlirtpoint()
     {
+        StartCoroutine(LoveIntheAir());
         CindySeducepoint++;
         //Debug.Log(CindySeducepoint);
     }
@@ -221,10 +241,23 @@ public class DialogueBox : MonoBehaviour
 
     void addEFlirtpoint()
     {
+        StartCoroutine(LoveIntheAir());
         EastonpointSeduce++;
         //Debug.Log(CindySeducepoint);
     }
 
+     void Resetbg()
+    {
+        //here we check if our love filters are on 
+        if (loveMode)
+        {
+            StartCoroutine(LoveErased());
+        }
+        if (akward)
+        {
+            StartCoroutine(Akgone());
+        }
+    }
 
     void PlayAudioLine(int selection)
     {
@@ -234,7 +267,10 @@ public class DialogueBox : MonoBehaviour
     }
 
 
-
+    void badSitution()
+    {
+        StartCoroutine(YoudoneGoof());
+    }
 
 
     void animatespriteChage()
@@ -298,5 +334,49 @@ public class DialogueBox : MonoBehaviour
     }
 }
 
+    IEnumerator LoveIntheAir()
+    {
+        loveMode = true;
+        for (float i = 0; i <= 1; i += (Time.deltaTime) * 4)
+        {
+            love.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+    IEnumerator LoveErased()
+    {
+        loveMode = false;
+        for (float i = 1; i >= 0; i -= (Time.deltaTime) * 4)
+        {
+            love.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+
+    IEnumerator Akgone()
+    {
+        akward = false;
+        for (float i = 0.7f; i >= 0; i -= (Time.deltaTime) * 4)
+        {
+
+            bgAkward.color = new Color(1, 1, 1, i);
+            faceAkward.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+    IEnumerator YoudoneGoof()
+    {
+        akward = true;
+        for (float i = 0; i <= 0.7f; i += (Time.deltaTime) * 4)
+        {
+
+            bgAkward.color = new Color(1, 1, 1, i);
+            faceAkward.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
 }
 
